0b6286cb76224ccd23cc4cb62fb74a1ab023a8f2
KhaledOsama17626 <71179264+KhaledOsama17626@users.noreply.github.com>
2020-09-15 05:12:09 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('google'),
+      ),
+      body: WebView(
+        initialUrl: 'https://brainfactoryeg.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
